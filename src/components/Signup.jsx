a81666ca0070080ac47359fdfd8864486ee49851
Cristina <kamuthucristina@gmail.com>
2025-07-07 11:40:34 +0300

    login page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { CiUser } from "react-icons/ci";
+import { TbLockPassword } from "react-icons/tb";
+import { TbPencilQuestion } from "react-icons/tb";
+
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='form-container'>
+        <h2>Login</h2>
+        <form>
+        <div>
+        <CiUser />
+        <input type='text' placeholder='enter your email or user email' />
+</div>
+<div>
+    <TbLockPassword />
+    <input type="password" placeholder=' Enter your password' />
+
+</div>
+<button>Sign in </button>
+</form>
+<div>
+  
+<p>Don't have an account please sign up</p> 
+  <TbPencilQuestion />
+
+
+</div>
+    </div>
   )
 }
 
